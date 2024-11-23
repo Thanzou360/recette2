ed3430d54985bd0a40001698a5c9e8368ddb16c6
thanzou <axelromain74@gmail.com>
2024-11-23 12:34:11 +0100

    cinq

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -17,11 +17,21 @@ function Search({setName,setData}) {
   }
 
   return (
-    <div className='flex items-center justify-center mt-20 mb-4'>
-        <input value={search} onChange={(e)=>{setSearch(e.target.value)}} className='border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none' placeholder='search'/>
-        <button onClick={onSearch} className='bg-black hover:bg-green-700 text-white  font-bold py-2 px-4 rounded'>
-            Search
-        </button>
+    <div className="flex items-center justify-center mt-20 mb-4">
+      <input
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className={`border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none ${
+          search.length > 0 ? 'w-full' : 'w-64' 
+        }`}
+        placeholder="search"
+      />
+      <button
+        onClick={onSearch}
+        className="bg-black hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Search
+      </button>
     </div>
   )
 }
